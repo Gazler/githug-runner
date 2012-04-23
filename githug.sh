3572dd3e4e6629c9a3c8fb39923e69bb0e21cbe4
Gary Rennie <webmaster@gazler.com>
2012-04-23 07:46:33 +0100

    update for level 34

--- githug.sh
+++ githug.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+##! /bin/bash
 rm -rf git_hug
 
 echo "y" | githug #create folder
@@ -47,99 +47,100 @@ echo $FILE_NAME
 git rm --cached $FILE_NAME
 githug
 
-level 11
+#level 11
 git mv oldfile.txt newfile.txt
 githug
 
-level 12
+#level 12
 git log | grep commit | cut -c 8-14 | githug
 
-level 13
+#level 13
 git tag new_tag
 githug
 
-level 14
+#level 14
 git add forgotten_file.rb
 git commit --amend -C HEAD
 githug
 
-level 15
+#level 15
 git reset HEAD to_commit_second.rb
 githug
 
-level 16
+#level 16
 git checkout -- config.rb
 githug
 
-level 17
+#level 17
 git remote | githug
 
-level 18
+#level 18
 git remote -v | tail -2 | head -1 | cut -c 17-52 | githug
 
-level 19
+#level 19
 git pull origin master
 githug
 
-level 20
+#level 20
 git remote add origin https://github.com/githug/githug
 githug
 
-level 21
+#level 21
 echo "26" | githug
 
-level 22
+#level 22
 echo "spider man" | githug
 
-level 23
+#level 23
 git branch test_code
 githug
 
-level 24
+#level 24
 git checkout -b my_branch
 githug
 
-level 25
+#level 25
 COMMIT=$(git log HEAD~1 | head -1 | cut -d " " -f 2)
 git branch test_branch $COMMIT
 githug
 
-level 26
+#level 26
 git merge feature
 githug
 
-level 27
+#level 27
 COMMIT=$(git log new-feature --oneline  -n 3 | tail -1 | cut -d " " -f 1)
 git cherry-pick $COMMIT
 githug
 
-level 28
+#level 28
 git rebase -i HEAD~2
 githug
 
-level 29
+#level 29
 git rebase -i HEAD~3
 githug
 
-level 30
+#level 30
 git merge --squash long-feature-branch
 git commit -m "Merged Long Feature Branch"
 githug
 
-level 31
+#level 31
 git rebase -i HEAD~3
 githug
 
-level 32
+#level 32
 git add -e
 githug
 
-level 33
+#level 33
 git checkout solve_world_hunger
 githug
 
 #level 34
-git revert HEAD~1
+sleep 1
+git revert HEAD~1 --no-edit
 githug
 
 #level 35
